aaf69a34fcabb2705157d03bdd4986a29b4ba6a3
VarishKapur <kapur.varish75@gmail.com>
2025-08-17 14:23:33 +0530

    Update q5.cpp

--- la1/q5.cpp
+++ la1/q5.cpp
@@ -1,24 +1,23 @@
 #include<iostream>
 using namespace std;
-
 int main() {
     int a[10][10], r, c;
 
-    cout << "Enter rows and columns: ";
-    cin >> r >> c;
-    for (int i = 0; i < r; i++)
-        for (int j = 0; j < c; j++)
-            cin >> a[i][j];
-    for (int i = 0; i < r; i++) {
-        int sum = 0;
-        for (int j = 0; j < c; j++)
-            sum += a[i][j];
-        cout << "Sum of row " << i<< sum << endl;
+    cout <<"Enter rows and columns: ";
+    cin>>r >>c;
+    for(int i= 0;i <r; i++)
+        for (int j =0; j< c; j++)
+            cin >>a[i][j];
+    for (int i =0;i< r; i++) {
+        int sum= 0;
+        for(int j= 0; j< c; j++)
+            sum +=a[i][j];
+        cout<<"Sum of row"<< i<< sum << endl;
     }
-       for (int j = 0; j < c; j++) {
-        int sum = 0;
-        for (int i = 0; i < r; i++)
-            sum += a[i][j];
-        cout << "Sum of column " << j<< sum << endl;
+       for(int j= 0;j< c; j++) {
+        int sum =0;
+        for(int i =0;i< r; i++)
+            sum+= a[i][j];
+        cout<<"Sum of column"<< j<< sum << endl;
     }
 }
