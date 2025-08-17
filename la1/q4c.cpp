c50ab121402bd197eef37938c5b99887240410c7
VarishKapur <kapur.varish75@gmail.com>
2025-08-17 14:21:24 +0530

    Update q4c.cpp

--- la1/q4c.cpp
+++ la1/q4c.cpp
@@ -5,16 +5,16 @@ int main() {
     cout<<"Enter rows and columns:";
     cin>>r>>c;
     cout<<"Enter the matrix you want to transpose:";
-    for(int i = 0; i< r; i++)
-        for (int j = 0; j < c; j++)
-            cin >> a[i][j];
-    for(int i = 0; i < r; i++)
-        for (int j = 0; j < c; j++)
-            t[j][i] = a[i][j];
+    for(int i =0; i< r; i++)
+        for(int j= 0;j < c;j++)
+            cin >>a[i][j];
+    for(int i =0;i < r;i++)
+        for(int j =0;j < c; j++)
+            t[j][i]= a[i][j];
     cout<<"Transpose matrix is below:";
-    for (int i =0;i < c;i++) {
-        for (int j= 0;j <r; j++)
-            cout<<t[i][j]<<" ";
+    for(int i =0;i< c;i++) {
+        for(int j= 0;j <r; j++)
+            cout<<t[i][j];
         cout<< endl;
     }
 }
