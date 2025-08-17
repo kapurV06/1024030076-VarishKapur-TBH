eb2cc70686a7b55e56e888b515b82f5f281a2d57
VarishKapur <kapur.varish75@gmail.com>
2025-08-17 14:15:30 +0530

    Update q4a.cpp

--- la1/q4a.cpp
+++ la1/q4a.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 int main() {
     int arr[100], n, temp;
-    cout <<"Enter number of elements: ";
+    cout <<"Enter number";
     cin>> n;
     for(int i = 0;i < n;i++)
         cin >> arr[i];
-    for (int i= 0;i <n / 2; i++) {
-        temp = arr[i];
-        arr[i] = arr[n - i - 1];
-        arr[n - i - 1] = temp;
+    for (int i= 0;i <n/2; i++) {
+        temp= arr[i];
+        arr[i] =arr[n - i- 1];
+        arr[n- i-1]= temp;
     }
-    cout << "Reversed array: ";
+    cout<<"Reversed array:";
     for (int i =0; i< n; i++)
-        cout<<arr[i] <<" ";
+        cout<<arr[i];
 }
